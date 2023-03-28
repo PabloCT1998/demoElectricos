7f92378b02526343d4d61e6d60d95b5b1f21dc9d
PabloCT1998 <43761330+PabloCT1998@users.noreply.github.com>
2023-03-28 11:59:59 -0600

    Update eliminarReq.php

--- eliminarReq.php
+++ eliminarReq.php
@@ -2,7 +2,6 @@
 include ('includes/funciones.php');
 session_start();
 $num = $_POST['req'];
-print_r($_SESSION['requisicion'][$num]);
 borrarReq($num);
 function borrarReq($num){
     unset($_SESSION['requisicion'][$num]);
